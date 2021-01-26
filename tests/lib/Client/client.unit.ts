7385ff56817dc1c707a356d933301341240d747b
Zhiheng Lu <brennan@voiceflow.com>
2021-01-26 17:02:53 -0500

    chore: setting up unit tests

---
+++ tests/lib/Client/client.unit.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+// import sinon from 'sinon';
+
+describe('client unit tests', () => {
+    it('hooked up the unit tests correctly', () => {
+        expect(true).to.eql(true);
+    })
+});
