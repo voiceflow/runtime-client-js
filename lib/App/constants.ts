3d14c5119bbbb575c5419af02808e001b530e893
Tyler Han <tyler@voiceflow.com>
2021-01-30 12:52:25 -0500

    chore: fix lint

--- lib/App/constants.ts
+++ lib/App/constants.ts
@@ -1,2 +1,2 @@
 export const SSML_TAG_REGEX = /<\/?[^>]+(>|$)/g;
-export const DEFAULT_ENDPOINT = 'https://general-runtime.voiceflow.com'
+export const DEFAULT_ENDPOINT = 'https://general-runtime.voiceflow.com';
