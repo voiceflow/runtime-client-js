1130f9ca13ae72f285b2ab95af3b12396b0d8c28
Zhi Heng (Brennan) Lu <32404412+zhihil@users.noreply.github.com>
2021-02-18 16:17:09 -0500

    fix: fixing html-parse-stringify import (CORE-5197) (#26)
    * fix: fixing html-parse-stringify import
    
    * style: linting
    
    Co-authored-by: Zhiheng Lu <brennan@voiceflow.com>

--- lib/Client/index.ts
+++ lib/Client/index.ts
@@ -4,6 +4,8 @@ import _cloneDeep from 'lodash/cloneDeep';
 
 import { RequestContext, ResponseContext } from '@/lib/types';
 
+import { adaptResponseContext } from './adapters';
+
 export type ClientConfig<S> = { variables?: Partial<S>; endpoint: string; versionID: string };
 
 class Client<S extends Record<string, any> = Record<string, any>> {
@@ -37,7 +39,10 @@ class Client<S extends Record<string, any> = Record<string, any>> {
   }
 
   async interact(body: RequestContext): Promise<ResponseContext> {
-    return this.axios.post(`/interact/${this.versionID}`, body).then((response) => response.data);
+    return this.axios
+      .post(`/interact/${this.versionID}`, body)
+      .then((response) => response.data)
+      .then((context) => adaptResponseContext(context));
   }
 
   getVersionID() {
