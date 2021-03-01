7a61a9c4158d3ef2d85a0418cb4e4d5d1ad770d7
Tyler Han <tyler@voiceflow.com>
2021-03-01 11:34:05 -0500

    fix: add index to handler

--- lib/Events/index.ts
+++ lib/Events/index.ts
@@ -4,9 +4,9 @@ import { GeneralTrace, TraceMap, TraceType } from '@/lib/types';
 
 import Context from '../Context';
 
-export type TraceEventHandler<T extends TraceType, V extends Record<string, any>> = (trace: TraceMap[T], context: Context<V>) => void;
+export type TraceEventHandler<T extends TraceType, V extends Record<string, any>> = (trace: TraceMap[T], context: Context<V>, index: number) => void;
 
-export type GeneralTraceEventHandler<V extends Record<string, any>> = (trace: GeneralTrace, context: Context<V>) => void;
+export type GeneralTraceEventHandler<V extends Record<string, any>> = (trace: GeneralTrace, context: Context<V>, index: number) => void;
 
 type _Map<T extends Record<string, any>, K extends TraceType = TraceType> = Map<K, Array<TraceEventHandler<K, T>>>;
 
@@ -43,12 +43,12 @@ export class EventManager<V extends Record<string, any>> {
   }
 
   async handle<T extends TraceType>(trace: TraceMap[T], context: Context<V>) {
-    await Bluebird.each(this.specHandlers.get(trace.type)!, async (handler: TraceEventHandler<T, V>) => {
-      await handler(trace, context);
+    await Bluebird.each(this.specHandlers.get(trace.type)!, async (handler: TraceEventHandler<T, V>, index) => {
+      await handler(trace, context, index);
     });
 
-    await Bluebird.each(this.genHandlers, async (handler: GeneralTraceEventHandler<V>) => {
-      await handler(trace, context);
+    await Bluebird.each(this.genHandlers, async (handler: GeneralTraceEventHandler<V>, index) => {
+      await handler(trace, context, index);
     });
   }
 }
